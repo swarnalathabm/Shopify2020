ee8752f4c16a8e114e16f1ea81f176d35673f073
Swarna <swarnaselvan.blr@gmail.com>
2020-09-10 13:24:03 +0530

    Account model and related files

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200908071754) do
+ActiveRecord::Schema.define(version: 20200910075112) do
 
-  create_table "product_details", force: :cascade do |t|
-    t.integer "productid"
-    t.string "productcategory"
-    t.string "productgroup"
-    t.string "productcode"
-    t.string "productdescription"
+  create_table "accounts", force: :cascade do |t|
+    t.string "shopify_account_url"
+    t.string "shopify_api_key"
+    t.string "shopify_password"
+    t.string "shopify_shared_secret"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
